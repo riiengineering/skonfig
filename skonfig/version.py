4272f0d552f4017c9ee7181c6755c583f7f1d297
Dennis Camera <dennis.camera@riiengineering.ch>
2026-06-23 21:33:56 +0200

    [skonfig.version] Implement support for got in addition to git

--- skonfig/version.py
+++ skonfig/version.py
@@ -51,36 +51,86 @@ def __silent_check_status(cmd, cwd):
 
 def __guess_git_version():
     import os
+    from datetime import (datetime, timezone)
 
     project_dir = os.path.dirname(os.path.dirname(__file__))
+    date_format = "%Y%m%d"
 
-    # If .git exists (could be a directory or a file in case of a submodule) it
-    # could be a Git repo, so try to generate version number from Git metadata.
+    version_date = datetime.today()
+    dev_id = None
+    version_suffix = ""
+
+    # if .git exists (directory or a file in case of a submodule) it could be
+    # a Git repo, so try to generate the version number from Git metadata.
     if os.path.exists(os.path.join(project_dir, ".git")):
-        # Try to use Git to generate the version
         try:
             tag = __silent_check_output(
                 ["git", "tag", "--points-at", "HEAD"],
                 project_dir)
 
-            if tag:
-                version = tag
-            else:
+            try:
+                version_date = datetime.strptime(tag, date_format)
+            except ValueError:
+                tag = None
+
+            if not tag:
                 commit_date = __silent_check_output(
-                    ["git", "show", "-s", "--pretty=format:%cs", "HEAD"],
+                    ["git", "show", "-s", "--pretty=format:%cI", "HEAD"],
                     project_dir)
-                version = "%s.dev%u" % (commit_date.replace("-", ""), 0)
+                version_date = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%S%:z")
+                dev_id = 0
 
             if 0 != __silent_check_status(
                     ["git", "diff-index", "--quiet", "HEAD", "--"],
                     project_dir):
-                version += "+dirty"
+                version_suffix += "+dirty"
+        except Exception:
+            pass
+    # if .got exists it is a Got working directory, so try to generate the
+    # version number from Git metadata.
+    elif os.path.exists(os.path.join(project_dir, ".got")):
+        try:
+            with open(os.path.join(project_dir, ".got", "base-commit")) as f:
+                base_commit = f.read().strip()
 
-            return version
+            import re
+            tag_re = re.compile(r"^[0-9]{4}(?:-[0-9]{2}){2} commit:([0-9a-f]+) (.*?):")
+            for line in __silent_check_output(["got", "tag", "-ls"], project_dir).splitlines():
+                tag_match = tag_re.match(line)
+                if tag_match is None:
+                    continue
+                if base_commit.startswith(tag_match[1]):
+                    try:
+                        version_date = datetime.strptime(tag_match[2], date_format)
+                        break
+                    except ValueError:
+                        pass
+            else:
+                for line in __silent_check_output(
+                        ["got", "log", "-c", base_commit, "-x", base_commit],
+                        project_dir).splitlines():
+                    if not line.strip():
+                        break
+                    try:
+                        (k, v) = line.split(": ", 1)
+                    except ValueError:
+                        continue
+
+                    if k == "date":
+                        version_date = datetime.strptime(v, "%a %b %d %H:%M:%S %Y %Z")
+                        dev_id = 0
+                        break
+
+            if __silent_check_output(["got", "status"], project_dir):
+                version_suffix += "+dirty"
         except Exception:
             pass
 
-    return "0+unknown"
+    return "%s%s%s" % (
+        version_date.astimezone(timezone.utc).strftime("%Y%m%d"),
+        ".dev%u" % dev_id if dev_id is not None else "",
+        version_suffix)
+
 
 
 VERSION = __guess_git_version()
